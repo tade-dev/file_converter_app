7d444412e104a438a269b10665bc919af1971e70
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-03-23 11:34:00 +0100

    Add font and color management classes; update pubspec.yaml for new assets

---
+++ lib/core/resources/colors_x.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+
+  static final primary = Color(0xff034844);
+  static final bgnColor = Color(0xffEDEFF2);
+  static final white = Color(0xffF9F9F9);
+  static final dividerColor = Color(0xffBCC6C5);
+  static final greenAlt = Color(0xff0C756F).withOpacity(0.10);
+  static final green= Color(0xff0C756F);
+  static final loading = Color(0xffFDCF6F);
+  static final loadingAlt = Color(0xffF0ECE1);
+  static final danger = Color(0xffFF6363);
+
+}
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,68 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconGen {
+  const $AssetsIconGen();
+
+  /// File path: assets/icon/Frame 47869.svg
+  String get frame47869 => 'assets/icon/Frame 47869.svg';
+
+  /// File path: assets/icon/Frame 47870.svg
+  String get frame47870 => 'assets/icon/Frame 47870.svg';
+
+  /// File path: assets/icon/crop.svg
+  String get crop => 'assets/icon/crop.svg';
+
+  /// File path: assets/icon/documents.svg
+  String get documents => 'assets/icon/documents.svg';
+
+  /// File path: assets/icon/expand.svg
+  String get expand => 'assets/icon/expand.svg';
+
+  /// File path: assets/icon/minimize.svg
+  String get minimize => 'assets/icon/minimize.svg';
+
+  /// File path: assets/icon/refresh_left.svg
+  String get refreshLeft => 'assets/icon/refresh_left.svg';
+
+  /// File path: assets/icon/refresh_right.svg
+  String get refreshRight => 'assets/icon/refresh_right.svg';
+
+  /// List of all assets
+  List<String> get values => [
+    frame47869,
+    frame47870,
+    crop,
+    documents,
+    expand,
+    minimize,
+    refreshLeft,
+    refreshRight,
+  ];
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/folder.svg
+  String get folder => 'assets/svg/folder.svg';
+
+  /// File path: assets/svg/folder_upload.svg
+  String get folderUpload => 'assets/svg/folder_upload.svg';
+
+  /// List of all assets
+  List<String> get values => [folder, folderUpload];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconGen icon = $AssetsIconGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
